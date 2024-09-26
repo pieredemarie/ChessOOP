10760b08d62a357a1aa249b445a51eb6fd4a40cf
Khachatryan Artem <65036324+pieredemarie@users.noreply.github.com>
2024-09-26 10:07:12 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -2,48 +2,66 @@
 
 class Board {
 protected:
-	Cell* board[8][8];
+	Cell board[8][8];
 public:
 	Board(Cell* board[8][8]);
 	void InitialiseBoard(Cell* board[8][8]);
 	bool isCheckMate(Cell* board[8][8]);
 	bool isCheck(Cell* board[8][8]);
+	void StartGame(Cell* board[8][8]);
 	void ArrangeFigures(Cell* board[8][8]);
 };
 class Cell :public Board {
-public:
+protected:
 	int x, y;
 	bool isFree;
+public:
 	Cell(int x, int y);
 	bool isFree(int x, int y);
 };
 class Figure {
-public: 
+protected:
 	int x, y;
-	enum class type { ROCK, PAWN, QUEEN, KING, KNIGHT };
+	enum class type { ROCK,BISHOP, PAWN, QUEEN, KING, KNIGHT };
 	enum class color { WHITE, BLACK };
+	type FigureType;
+	color FigureColor;
 public:
 	Figure(int x, int y, type TYPE, color COLOR);
-	bool CanMoveFigure(int newX, int newY);
+	virtual bool CanMoveFigure(int newX, int newY);
 	void MoveFigure(int newX, int newY);
 };
 class Knight : public Figure {
-	int dx, dy;
+public:
+	Knight(int x, int y, color COLOR) :Figure(x, y, type::KNIGHT, COLOR) {};
+	bool CanMoveFigure(int dx, int dy) override;
 };
 class Rock : public Figure {
-	int dx, dy;
+public:
+	Rock(int x, int y, color COLOR) :Figure(x, y, type::ROCK, COLOR) {};
+	bool CanMoveFigure(int dx, int dy) override;
 };
 class Pawn : public Figure {
-	int dx, dy;
+protected:
+	bool PawnTurn;//Превращение пешки
+public:
+	Pawn(int x, int y,bool PawnTurn, color COLOR) :Figure(x, y, type::PAWN, COLOR) {};
+	bool CanMoveFigure(int dx, int dy) override;
 };
 class Queen : public Figure {
-	int dx, dy;
+	Queen(int x, int y, color COLOR) :Figure(x, y, type::QUEEN, COLOR) {};
+	bool CanMoveFigure(int dx, int dy) override;
 };
 class King : public Figure {
-	int dx, dy;
+protected:
+	bool canCastle;//Рокировка
+public:
+	King(int x, int y, bool canCastle, color COLOR) :Figure(x, y, type::KING, COLOR) {};
+	bool CanMoveFigure(int dx, int dy) override;
 };
 class Bishop :public Figure {
-	int dx, dy;
+	Bishop(int x, int y, color COLOR) :Figure(x, y, type::BISHOP, COLOR) {};
+	bool CanMoveFigure(int dx, int dy) override;
 };
 
 bool isFree(bool isFree) {
