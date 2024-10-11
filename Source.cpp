1870937038f54694bf40615f2f4c64d194654504
Khachatryan Artem <65036324+pieredemarie@users.noreply.github.com>
2024-10-11 09:19:25 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,143 +1,55 @@
 #include <iostream>
 #include <math.h>
-
-class Figure {
-public:
-	int x, y;
-	enum class type { ROCK, BISHOP, PAWN, QUEEN, KING, KNIGHT, NONE };
-	enum class color { WHITE, BLACK , NONE};
-	type FigureType;
-	color FigureColor;
-public:
-	Figure(int x, int y, type TYPE, color COLOR) {
-		this->x = x;
-		this->y = y;
-		FigureType = TYPE;
-		FigureColor = COLOR;
-	};
-	//virtual bool CanMoveFigure(int newX, int newY);
-	void MoveFigure(int newX, int newY);
-	int getX(int x);
-	int getY(int y);
-	bool getColor(color FigureColor);
-};
-class Cell {
-public:
-	int x, y;
-	Figure figure;
-	bool isFree;
-	bool isAttacked;
-public:
-	Cell(int x, int y, Figure figure, bool isFree) :
-		x(x), y(y), figure(figure), isFree(isFree), isAttacked(false) {}
-	bool isCellFree(bool isFree) {
-		return isFree;
-	};
-};
-class Board {
-public:
-	Cell* board[8][8];
-	bool WhiteTurn;
-public:
-	Board(Cell* board[8][8], bool WhiteTurn) {
-		for (int i = 0; i < 8; ++i) {
-			for (int j = 0; j < 8; ++j) {
-				this->board[i][j] = board[i][j];
-			}
-		}
-	}
-	bool isCheckMate(Cell* board[8][8]);
-	bool isCheck(Cell* board[8][8]);
-	void ArrangeFigures(Cell* board[8][8]);
-	void PrintBoard(Cell* board[8][8]);
-};
-class Knight :public Figure {
-public:
-	Knight(int x, int y, color COLOR) :Figure(x, y, type::KNIGHT, COLOR) {};
-	//bool CanMoveFigure(int dx, int dy) override;
-};
-class Rock :public Figure {
-public:
-	Rock(int x, int y, color COLOR) :Figure(x, y, type::ROCK, COLOR) {
-	
-	};
-	//bool CanMoveFigure(int dx, int dy) override;
-};
-class Pawn :public Figure {
-protected:
-	bool PawnTurn;//Превращение пешки
-	bool CanTwoCellsGo;
-public:
-	Pawn(int x, int y, bool PawnTurn, bool CanTwoCellsGo, color COLOR) :Figure(x, y, type::PAWN, COLOR) {};
-	//Bool CanMoveFigure(int dx, int dy) override;
-};
-class Queen : public Figure {
-public:
-	Queen(int x, int y, color COLOR) :Figure(x, y, type::QUEEN, COLOR) {};
-	//bool CanMoveFigure(int dx, int dy) override;
-};
-class King :public Figure {
-protected:
-	bool canCastle;//Рокировка
-public:
-	King(int x, int y, bool canCastle, color COLOR) :Figure(x, y, type::KING, COLOR) {};
-	//bool CanMoveFigure(int dx, int dy) override;
-};
-class Bishop :public Figure {
-public:
-	Bishop(int x, int y, color COLOR) :Figure(x, y, type::BISHOP, COLOR) {};
-	//bool CanMoveFigure(int dx, int dy) override;
-};
-
-/*bool Knight::CanMoveFigure(int dx, int dy) {
-	return (abs(x - dx) * abs(y - dy)) == 2;
+#include <locale.h>
+#include "Classes.h"
+// класс Board
+bool Board::isCellOccupied(int dx, int dy) {
+	return !(board[dx][dy]->isCellFree());
 }
-*/
 void Board::PrintBoard(Cell* board[8][8]) {
-	std::cout << "   A   B   C   D   E   F   G   H" << std::endl;
-	std::cout << " ---------------------------------" << std::endl;
-
+	std::cout << "    A   B   C   D   E   F   G   H   " << std::endl;
+	std::cout << " -----------------------------------" << std::endl;
 	for (int i = 7; i >= 0; i--) {
-		std::cout << i+1 << " |";
-		for (int j = 0; j < 8; ++j) {
-			// Проверяем, есть ли фигура в ячейке
-			if (!board[j][i]->isFree) { // Используем !isFree, чтобы проверить, есть ли фигура
-				// Выводим символ фигуры
-				switch (board[j][i]->figure.FigureType) {
+		std::cout << i + 1 << " |";
+		for (int j = 0; j < 8; j++) {
+			if (!board[i][j]->isFree) {
+				switch (board[i][j]->figure.FigureType) {
 				case Figure::type::PAWN:
-					std::cout << (board[j][i]->figure.FigureColor == Figure::color::WHITE ? "P" : "p") << "  |";
+					std::cout << (board[i][j]->figure.FigureColor == Figure::color::WHITE ? "P" : "p") << "  |";
 					break;
 				case Figure::type::ROCK:
-					std::cout << (board[j][i]->figure.FigureColor == Figure::color::WHITE ? "R" : "r") << "  |";
+					std::cout << (board[i][j]->figure.FigureColor == Figure::color::WHITE ? "R" : "r") << "  |";
 					break;
 				case Figure::type::KNIGHT:
-					std::cout << (board[j][i]->figure.FigureColor == Figure::color::WHITE ? "N" : "n") << "  |";
+					std::cout << (board[i][j]->figure.FigureColor == Figure::color::WHITE ? "N" : "n") << "  |";
 					break;
 				case Figure::type::BISHOP:
-					std::cout << (board[j][i]->figure.FigureColor == Figure::color::WHITE ? "B" : "b") << "  |";
+					std::cout << (board[i][j]->figure.FigureColor == Figure::color::WHITE ? "B" : "b") << "  |";
 					break;
 				case Figure::type::QUEEN:
-					std::cout << (board[j][i]->figure.FigureColor == Figure::color::WHITE ? "Q" : "q") << "  |";
+					std::cout << (board[i][j]->figure.FigureColor == Figure::color::WHITE ? "Q" : "q") << "  |";
 					break;
 				case Figure::type::KING:
-					std::cout << (board[j][i]->figure.FigureColor == Figure::color::WHITE ? "K" : "k") << "  |";
+					std::cout << (board[i][j]->figure.FigureColor == Figure::color::WHITE ? "K" : "k") << "  |";
 					break;
 				}
 			}
 			else {
-				std::cout << "   |"; // Если фигуры нет, выводим пустую ячейку
+				std::cout << "   |";
 			}
 		}
+		std::cout << i + 1;
 		std::cout << std::endl;
-		std::cout << " ---------------------------------" << std::endl;
+		std::cout << " -----------------------------------" << std::endl;
 	}
+	std::cout << "    A   B   C   D   E   F   G   H   " << std::endl;
 }
 void Board::ArrangeFigures(Cell* board[8][8]) {
 	board[0][0]->figure = *new Rock(0, 0, Figure::color::WHITE);
 	board[0][1]->figure = Knight(0, 0, Figure::color::WHITE);
 	board[0][2]->figure = Bishop(0, 0, Figure::color::WHITE);
 	board[0][3]->figure = Queen(0, 0, Figure::color::WHITE);
-	board[0][4]->figure = King(0, 0,true, Figure::color::WHITE);
+	board[0][4]->figure = King(0, 0, true, Figure::color::WHITE);
 	board[0][5]->figure = Bishop(0, 0, Figure::color::WHITE);
 	board[0][6]->figure = Knight(0, 0, Figure::color::WHITE);
 	board[0][7]->figure = Rock(0, 0, Figure::color::WHITE);
@@ -155,7 +67,7 @@ void Board::ArrangeFigures(Cell* board[8][8]) {
 	board[7][5]->figure = Bishop(0, 0, Figure::color::BLACK);
 	board[7][6]->figure = Knight(0, 0, Figure::color::BLACK);
 	board[7][7]->figure = Rock(0, 0, Figure::color::BLACK);
-	
+
 	for (int i = 2; i < 6; i++) {
 		for (int j = 0; j < 8; j++) {
 			board[i][j]->isFree = true;
@@ -168,16 +80,42 @@ void Board::ArrangeFigures(Cell* board[8][8]) {
 		board[6][i]->isFree = false;
 	}
 }
-
+//
+// дочерние классы Figure
+bool Knight::CanMoveFigure(int dx, int dy) {
+	return (abs(x - dx) * abs(y - dy)) == 2;
+}
+bool Pawn::CanMoveFigure(int dx, int dy, bool CanTwoCellsGo) {
+	if (CanTwoCellsGo) {
+		return (abs(dx - x) == 2 && (dy == y));
+	}
+	else {
+		return (abs(dx - x) == 1 && (dy == y));
+	}
+}
+bool Bishop::CanMoveFigure(int dx, int dy) {
+	return (abs(dx - x) == abs(dy - y));
+}
+bool Queen::CanMoveFigure(int dx, int dy) {
+	return ((abs(dx - x) == abs(dy - y)) || ((x == dx) && (y != dy)) || ((dy == y) && (x != dx)));
+}
+bool King::CanMoveFigure(int dx, int dy) {
+	return ((abs(dx - x) == 1) || (abs(dy - y)) || (abs(dy - y) && abs(dx - x)));
+}
+bool Rock::CanMoveFigure(int dx, int dy) {
+	return (((x == dx) && (y != dy)) || ((dy == y) && (x != dx)));
+}
 int main() {
-	std::cout << "test" << std::endl;
+	setlocale(LC_ALL, "rus");
+	std::cout << "p - пешка\nr - ладья\nn - конь\nb - слон\nk - король\nq - ферзь" << std::endl;
+	std::cout << "Заглавные буквы - белые фигуры, строчные - черные" << std::endl << std::endl << std::endl;
 	Cell* board[8][8];
-      for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 8; j++) {
-          board[i][j] = new Cell(i, j, Figure(i,j,Figure::type::NONE, Figure::color::NONE), true); // Создаем Cell с пустой фигурой
-        }
-      }
-      Board chess(board, true); 
-      chess.ArrangeFigures(board); 
-	  chess.PrintBoard(board);
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			board[i][j] = new Cell(i, j, Figure(i, j, Figure::type::NONE, Figure::color::NONE), true); // Создаем Cell с пустой фигурой
+		}
+	}
+	Board chess(board, true);
+	chess.ArrangeFigures(board);
+	chess.PrintBoard(board);
 }
